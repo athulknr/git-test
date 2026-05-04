ba2ee60d1590c77d3580d22ca4a3534b5fbc4d5d
ATHUL K <122529031+athulknr@users.noreply.github.com>
2026-05-04 11:25:35 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,6 @@ public class App {
     public static int add(int a, int b) {
         return a + b;
     }
-
     public static void main(String[] args) {
         System.out.println("Hello");
     }
